7d10daae033383910641cb7c86eeb823b6072276
Michael Kelly <kikemelly@gmail.com>
2023-03-22 15:41:11 +0700

    Update myplugin.php

--- plugins/myplugin.php
+++ plugins/myplugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       A basic plugin
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Version:           1.10.4
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            John Smith
